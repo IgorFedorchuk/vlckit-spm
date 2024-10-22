de9ef76a9c5d6a294afa11619e06c73e2473fc4e
Igor <>
2024-10-22 08:42:06 +0200

    update version

--- generate.sh
+++ generate.sh
@@ -2,10 +2,10 @@
 #
 rm -rf .tmp/ || true
 
-TAG_VERSION="v3.6.0.b11"
-IOS_URL="https://download.videolan.org/cocoapods/unstable/MobileVLCKit-3.6.0b11-fbffb2f1-6d82c5af.tar.xz"
-MACOS_URL="https://download.videolan.org/cocoapods/unstable/VLCKit-3.6.0b11-fbffb2f1-6d82c5af.tar.xz"
-TVOS_URL="https://download.videolan.org/cocoapods/unstable/TVVLCKit-3.6.0b11-fbffb2f1-6d82c5af.tar.xz"
+TAG_VERSION="v3.6.0"
+IOS_URL="https://download.videolan.org/cocoapods/prod/MobileVLCKit-3.6.0-c73b779f-dd8bfdba.tar.xz "
+MACOS_URL="https://download.videolan.org/cocoapods/prod/VLCKit-3.6.0-c73b779f-dd8bfdba.tar.xz"
+TVOS_URL="https://download.videolan.org/cocoapods/prod/TVVLCKit-3.6.0-c73b779f-dd8bfdba.tar.xz"
 
 mkdir .tmp/
 
